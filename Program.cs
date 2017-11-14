968e7cf3f800677eb9422d5cb5f6a5f9ea04b9ae
Panda <giorgi.anakidze.1@btu.edu.ge>
2017-11-13 17:48:18 -0800

    comments

--- Program.cs
+++ Program.cs
@@ -4,38 +4,49 @@ namespace ConsoleApp1
 {
     class Program
     {
-        // Cont - переменная позволяющая продолжить игру
-        //
+        // cont - Will use later for continuing game.
+
         static bool cont = true;
         static void Main()
         {
-            // начальные очки
-            int score = 0;
+            // Points at start.
+
+            int points = 0;
             while (cont == true)
             {
                 string charlist = "Harry, Ron, Hermione, Luna, Severus, Voldemort, Albus";
                 string[] s = "Harry|Ron|Hermione|Luna|Severus|Voldemort|Albus".Split('|');
-                // рандом последством переменной rnd
+
+                // Choosing random character.
+
                 Random rnd = new Random();
                 string r = s[rnd.Next(s.Length)];
                 Console.WriteLine("Welcome To The Guess The Character Game");
                 Console.WriteLine("Version - v0.3");
                 Console.WriteLine("Guess between these characters - {0}", charlist);
 
+                // Giving 5 attempts at start.
+
                 for (int i = 5; i > 0; i--)
                 {
                     Console.WriteLine("You've got {0} attempts", i);
+
+                    // Declaring userchar to accept user input and then use it in if/else.
+
                     string userchar;
                     Console.WriteLine("Your Character - ");
                     userchar = Console.ReadLine();
+
+                    // Checking if user input & randomly choosen string are the same.
+                    // In order if they are the same, you win, get 1 point and you are able to continue game with saving your points
                     if (userchar == r)
                     {
-                        score++;
+                        points++;
                         i = 5;
                         Console.WriteLine("Random character is - {0}", r);
                         Console.WriteLine("User character is - {0}", userchar);
                         Console.WriteLine("You Win!");
-                        Console.WriteLine("Score - {0}", score);
+                        Console.WriteLine("Score - {0}", points);
                         Console.WriteLine("Do you want to continue? [y] ( Any other key terminates process )");
                         ConsoleKeyInfo cki = Console.ReadKey();
                         if (cki.Key == ConsoleKey.Y)
@@ -52,9 +63,10 @@ namespace ConsoleApp1
                             break;
                         }
                     }
+                    // If randomly chosen character is Voldemort, you lose.
                     else if (r == "Voldemort")
                     {
-                        score = 0;
+                        points = 0;
                         i = 0;
                         Console.WriteLine("Voldemort has come!");
                         Console.WriteLine("You Lose!");
@@ -73,11 +85,12 @@ namespace ConsoleApp1
                             break;
                         }
                     }
+                    // If you run out of attempts you get message about this and you are able to try again.
                     while (i == 1 && userchar != r)
                     {
-                        score = 0;
+                        points = 0;
                         Console.WriteLine("You've run out of attempts!");
-                        Console.WriteLine("Score - {0}", score);
+                        Console.WriteLine("Score - {0}", points);
                         Console.WriteLine("You lose!");
                         Console.WriteLine("Do you want to try again? [y] ( Any other key terminates process )");
                         ConsoleKeyInfo ck = Console.ReadKey();
